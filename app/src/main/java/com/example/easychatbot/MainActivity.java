ec6e1fd2e4132b906f7467c8d28de562ef8fb430
Praveen Gupta <praveengupta221715@gmail.com>
2023-10-29 20:02:45 +0530

    Update MainActivity.java
    token removed

--- app/src/main/java/com/example/easychatbot/MainActivity.java
+++ app/src/main/java/com/example/easychatbot/MainActivity.java
@@ -109,7 +109,7 @@ public class MainActivity extends AppCompatActivity {
        RequestBody body = RequestBody.create(jsonBody.toString(),JSON);
         Request request = new Request.Builder()
                 .url("https://api.openai.com/v1/completions")
-                .header("Authorization", "Bearer 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")
+                .header("Authorization", "Bearer ")
                 .post(body)
                 .build();
 
